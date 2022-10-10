db8ddac4f6b5c5b1804f7e7f6859ee95b2629a5d
Deonte McCoy <deontemccoy19@gmail.com>
2022-10-10 14:53:34 -0500

    Add search button eventlistener and blank search condition

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './style.css';
+import "./style.css";
+import { getWeatherData } from "./functions";
+
+(() => {
+  const searchButton = document.querySelector("#searchButton");
+  searchButton.addEventListener("click", () => {
+    const city = document.querySelector("#city").value;
+    if (city) {
+      getWeatherData(city);
+    }
+  });
+})();
