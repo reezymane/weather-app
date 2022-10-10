9bdae1eae9b6702638f12a7626602ec18db507a9
Deonte McCoy <deontemccoy19@gmail.com>
2022-10-10 14:52:25 -0500

    Combine functions and add try/catch

--- src/functions.js
+++ src/functions.js
@@ -1,18 +1,12 @@
 import { weatherObject } from "./factories";
 
-function getAndStoreWeather(location) {
-  // Returns weather data promise from API
-  async function getWeatherData(city) {
+// Returns weather data promise from API
+async function getWeatherData(city) {
+  try {
     const locationString = city.split(" ").join("");
     const locationURL = `http://api.openweathermap.org/data/2.5/weather?q=${locationString}&APPID=f5c0b9e03265d5430f5da6709ce02b0f`;
     const response = await fetch(locationURL, { mode: "cors" });
-    return response;
-  }
-
-  // Processes the JSON data
-  async function processJSON(toSearch) {
-    const weatherData = await getWeatherData(toSearch);
-    const weatherJSON = await weatherData.json();
+    const weatherJSON = await response.json();
 
     // Store data into weatherObject
     weatherObject.name = weatherJSON.name;
@@ -26,9 +20,9 @@ function getAndStoreWeather(location) {
     weatherObject.dateTime = weatherJSON.dt;
     weatherObject.sunrise = weatherJSON.sys.sunrise;
     weatherObject.sunset = weatherJSON.sys.sunset;
+  } catch (error) {
+    console.log("ENTER NEW CITY FOO!");
   }
-
-  processJSON(location);
 }
 
-export { getAndStoreWeather };
+export { getWeatherData };
